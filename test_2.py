605f7432272162cf534eaf7b921bd6550ce4becf
KikatoMikishi <ian.the.wim@gmail.com>
2021-04-06 16:38:35 +0200

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,5 +1,6 @@
 from astropy.io import fits
-import numpy
+import numpy as np
+from tifffile import imsave
 
 #define hdul
 image_position = "fits/first_fit.fit"
@@ -7,14 +8,37 @@ hdul = fits.open(image_position)
 
 #get resolution and check if resolution is divisible by 2 and is odd
 def check_res(res):
-    if (res % 2) == 0:
-        return res
-    else:
-        res -= 1
-        check_res(res)
+    while (res % 2) != 0:
+        res = res - 1
+    return(res)
 
 resx = check_res(hdul[0].header[3])
 resy = check_res(hdul[0].header[4])
 
-#define data as an array
 data = hdul[0].data
+
+
+fact = 1
+
+output = []
+
+for i in range(int(resy / 2)):
+    xes = []
+    for j in range(int(resx / 2)):
+        #bggr
+        xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
+        #rggb
+        #xes.append((data[i*2-1][j*2-1], (data[i*2][j*2-1] + data[i*2-1][j*2])/2, data[i*2][j*2]*fact))
+    if (i % 100) == 0:
+        print(i)
+    output.append(xes)
+    
+outArray = np.array(output)
+
+imsave('test.tif', outArray)
+
+
+
+
+
+
