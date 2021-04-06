e3870109124bd0db63052c7c7a4bee7c0e215ba0
Matdio <maettu141@gmail.com>
2021-04-06 16:53:29 +0200

    asd

--- test_2.py
+++ test_2.py
@@ -26,9 +26,11 @@ for i in range(int(resy / 2)):
     xes = []
     for j in range(int(resx / 2)):
         #bggr
-        xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
+        #xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
         #rggb
         #xes.append((data[i*2-1][j*2-1], (data[i*2][j*2-1] + data[i*2-1][j*2])/2, data[i*2][j*2]*fact))
+        #blue
+        xes.append(int(data[i*2][j*2]*fact))
     if (i % 100) == 0:
         print(i)
     output.append(xes)
