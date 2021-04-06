cfd705e152e7fc1ac71e601d6241b94d481f0848
Matdio <maettu141@gmail.com>
2021-04-06 14:48:54 +0200

    Update test_ian.py

--- test_ian.py
+++ test_ian.py
@@ -1,12 +1,11 @@
 from astropy.io import fits
-import numpy
+import numpy as np
 
 image_position = "fits/first_fit.fit"
 hdul = fits.open(image_position)
 hdul.info()
 
 data = hdul[0].data
-print(data)
 
 
 
@@ -17,8 +16,15 @@ fact = 1
 
 output = []
 
-for i in range(resx/2):
+for i in range(int(resy/2)):
     xes = []
-    for j in range(resy/2):
-        xes.append((data[i*2][j*2]*fact, (data[i*2][j*2-1] + data[i*2-1][j*2])/2, data[i*2-1][j*2-1]))
+    for j in range(int(resx/2)):
+        #bggr
+        xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
+        #rggb
+        #xes.append((data[i*2-1][j*2-1], (data[i*2][j*2-1] + data[i*2-1][j*2])/2, data[i*2][j*2]*fact))
+        
     output.append(xes)
+    
+    
+outArray = np.array(output)  
