68533b90576e2cc77e2a88db2cb18768145b55e5
KikatoMikishi <ian.the.wim@gmail.com>
2021-04-07 11:54:46 +0200

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,7 +1,27 @@
 from astropy.io import fits
 import numpy as np
-from tifffile import imsave
-#from matplotlib import pyplot
+import tifffile as tiff
+
+def rgb_to_hsv(r, g, b):
+    rc, gc, bc = r/255, g/255, b/255
+    cmax = max(rc, gc, bc)
+    cmin = min(rc, gc, bc)
+    dc = cmax - cmin
+    
+    if rc == cmax:
+        h = 60*(((gc-bc)/dc)%6)
+    elif gc == cmax:
+        h = 60*(((bc-rc)/dc)+2)
+    elif bc == cmax:
+        h = 60*(((rc-gc)/dc)+4)
+    elif dc == 0:
+        h = 0
+    
+    s = (0 if cmax==0 else dc/cmax)
+    
+    v = cmax
+    
+    return((h, s, v))
 
 #define hdul
 image_position = "fits/first_fit.fit"
@@ -17,35 +37,29 @@ resx = check_res(hdul[0].header[3])
 resy = check_res(hdul[0].header[4])
 
 data = hdul[0].data
-print(data)
-quit()
 
 
+bitn = 16
 
+#fact = (2**16-1) / (2**bitn-1)
 fact = 1
-
 output = []
 
 for i in range(int(resy / 2)):
     xes = []
     for j in range(int(resx / 2)):
         #bggr
-        xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
+        #xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
         #rggb
-        #xes.append((data[i*2-1][j*2-1], (data[i*2][j*2-1] + data[i*2-1][j*2])/2, data[i*2][j*2]*fact))
+        xes.append(rgb_to_hsv(data[i*2-1][j*2-1]*fact, (data[i*2][j*2-1] + data[i*2-1][j*2])/2*fact, data[i*2][j*2]*fact))
+        
         #blue
         #xes.append(int(data[i*2][j*2]*fact))
     if (i % 100) == 0:
         print(i)
     output.append(xes)
     
-outArray = np.array(output)
-#print(outArray)
-imwrite('temp.tif', data, photometric='rgb')
+outArray = np.array(output, "uint" + str(bitn))
+print(outArray)
 
-
-
-
-
-
-
+tiff.imwrite('finite.tif', outArray, photometric='rgb')
