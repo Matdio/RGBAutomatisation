15cd5dea10d8e1de2526cf9ea06dd2760ce66e0c
Matdio <maettu141@gmail.com>
2021-04-07 12:00:33 +0200

    dfx

--- finite.py
+++ finite.py
@@ -18,26 +18,33 @@ resy = check_res(hdul[0].header[4])
 data = hdul[0].data
 
 
-bitn = 16
+bitn = 8
 
-fact = (2**16-1) / (2**bitn-1)
-fact = 1
-output = []
+fact =  (2**bitn-1) / (2**16-1)
+print(fact)
 
-for i in range(int(resy / 2)):
-    xes = []
-    for j in range(int(resx / 2)):
-        #bggr
-        #xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
-        #rggb
-        xes.append((data[i*2-1][j*2-1]*fact, (data[i*2][j*2-1] + data[i*2-1][j*2])/2*fact, data[i*2][j*2]*fact))
-        #blue
-        #xes.append(int(data[i*2][j*2]*fact))
-    if (i % 100) == 0:
-        print(i)
-    output.append(xes)
-    
-outArray = np.array(output, "uint" + str(bitn))
+
+
+def RGBTransform(resx, resy, data, fact):
+    output = []
+    for i in range(int(resy / 2)):
+        xes = []
+        for j in range(int(resx / 2)):
+            #bggr
+            #xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
+            #rggb
+            xes.append((data[i*2-1][j*2-1]*fact, ((data[i*2][j*2-1] + data[i*2-1][j*2])/2)*fact, data[i*2][j*2]*fact))
+            #blue
+            #xes.append(int(data[i*2][j*2]*fact))
+        if (i % 100) == 0:
+            print(i)
+        output.append(xes)
+    return output
+
+
+output = RGBTransform(resx, resy, data, fact)
+outArray = np.array(output, "uint8")
 print(outArray)
+print(outArray.dtype)
 
 tiff.imwrite('finite.tif', outArray, photometric='rgb')
