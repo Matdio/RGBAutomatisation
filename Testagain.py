2913e5244eed9548ab3451ea0dc8111a79be6619
Matdio <maettu141@gmail.com>
2021-04-07 09:32:48 +0200

    gs

--- Testagain.py
+++ Testagain.py
@@ -1,5 +1,5 @@
 import numpy as np
-from tifffile import imsave
+import tifffile as tiff
 
 maxx = 100
 maxy = 100
@@ -10,13 +10,13 @@ for i in range(maxx):
     t = []
     for j in range(maxy):
         if j < (maxy/2):
-            t.append(65000)
+            t.append((65000,65000,65000))
         else:
-            t.append(0)
+            t.append((0,0,0))
     outArray.append(t)
     
 outArray = np.array(outArray)
-        
+print(outArray)        
 
-imsave("trying.tif", outArray)
+tiff.imwrite("trying.tif", data = outArray)
         
--- test_2.py
+++ test_2.py
@@ -16,6 +16,9 @@ resx = check_res(hdul[0].header[3])
 resy = check_res(hdul[0].header[4])
 
 data = hdul[0].data
+print(data)
+quit()
+
 
 
 fact = 1
@@ -26,14 +29,13 @@ for i in range(int(resy / 2)):
     xes = []
     for j in range(int(resx / 2)):
         #bggr
-        #xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
+        xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
         #rggb
         #xes.append((data[i*2-1][j*2-1], (data[i*2][j*2-1] + data[i*2-1][j*2])/2, data[i*2][j*2]*fact))
         #blue
-        xes.append(int(data[i*2][j*2]*fact))
+        #xes.append(int(data[i*2][j*2]*fact))
     if (i % 100) == 0:
         print(i)
-        print(xes)
     output.append(xes)
     
 outArray = np.array(output)
