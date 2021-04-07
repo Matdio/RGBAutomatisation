f4284cdb94d5b0b90ef872198610b41a9fdcd4f4
KikatoMikishi <ian.the.wim@gmail.com>
2021-04-07 14:19:41 +0200

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -2,72 +2,61 @@ from astropy.io import fits
 import numpy as np
 import tifffile as tiff
 
-def rgb_to_hsv(r, g, b):
-    rc, gc, bc = r/255, g/255, b/255
-    cmax = max(rc, gc, bc)
-    cmin = min(rc, gc, bc)
-    dc = cmax - cmin
-    
-    if rc == cmax:
-        h = 60*(((gc-bc)/dc)%6)
-    elif gc == cmax:
-        h = 60*(((bc-rc)/dc)+2)
-    elif bc == cmax:
-        h = 60*(((rc-gc)/dc)+4)
-    elif dc == 0:
-        h = 0
-    
-    s = (0 if cmax==0 else dc/cmax)
-    
-    v = cmax
-    
-    return((h, s, v))
+#variables
+bitn = 16
+bitIn = 16
 
-#define hdul
+m = 0.05
+
+output = []
 image_position = "fits/first_fit.fit"
-hdul = fits.open(image_position)
 
-#get resolution and check if resolution is divisible by 2 and is odd
+#check if resolution is divisible by 2 and is odd
 def check_res(res):
     while (res % 2) != 0:
         res = res - 1
     return(res)
 
-resx = check_res(hdul[0].header[3])
-resy = check_res(hdul[0].header[4])
+#define hdul, get resolution, get data
+def needed_data():
+    global resx, resy, data
+    hdul = fits.open(image_position)
+    resx = check_res(hdul[0].header[3])
+    resy = check_res(hdul[0].header[4])
+    data = hdul[0].data
 
-data = hdul[0].data
+#function for mapping
+def mappingFunction(x, m):
+    if x == 0:
+        return 0
+    elif x == m:
+        return 0.5
+    elif x == 1:
+        return 1
+    else:
+        return ((m-1)*x)/((((2*m)-1)*x)-m)
 
+#RGB Transform and Stretch
+def TransformStretch(resx, resy, data, bitIn, m):
+    output = []
+    fact = 1/(2**bitIn)
+    for i in range(int(resy / 2)):
+        xes = []
+        for j in range(int(resx / 2)):
+            r = mappingFunction(data[i*2-1][j*2-1]*fact, m)*(2**bitIn)
+            g = mappingFunction(((data[i*2][j*2-1] + data[i*2-1][j*2])/2)*fact, m)*(2**bitIn)
+            b = mappingFunction(data[i*2][j*2]*fact, m)*(2**bitIn)
+            xes.append((r, g, b))
+            
+        if (i % 100) == 0:
+            print(i)
+        output.append(xes)
+    return output
 
-bitn = 16
+needed_data()
+output = TransformStretch(resx, resy, data, bitIn, m)
 
-fact = (2**16-1) / (2**bitn-1)
-#fact = 1
-output = []
-
-def functions(x1, x2, x3):
-    x1 = (2**bitn)*x1**0.5
-    x2 = (2**bitn)*x2**0.5
-    x3 = (2**bitn)*x3**0.5
-    return(x1, x2, x3)
-
-for i in range(int(resy / 2)):
-    xes = []
-    for j in range(int(resx / 2)):
-        #bggr
-        #xes.append((int(data[i*2][j*2]*fact), int((data[i*2][j*2-1] + data[i*2-1][j*2])/2), int(data[i*2-1][j*2-1])))
-        #rggb
-        xes.append(functions(data[i*2-1][j*2-1]*fact, (data[i*2][j*2-1] + data[i*2-1][j*2])/2*fact, data[i*2][j*2]*fact))
-        #blue
-        #xes.append(int(data[i*2][j*2]*fact))
-    if (i % 100) == 0:
-        print(i)
-    output.append(xes)
-
-
-    
-
-outArray = np.array(output, "uint" + str(bitn))
+outArray = np.array(output, "uint16")
 print(outArray)
 
 tiff.imwrite('finite.tif', outArray, photometric='rgb')
