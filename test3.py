d61c5cc69ac1454e2d34561bdefc89ed0afd0bab
KikatoMikishi <ian.the.wim@gmail.com>
2021-04-09 11:40:14 +0200

    s

--- test3.py
+++ test3.py
@@ -2,8 +2,9 @@ from astropy.io import fits
 import numpy as np
 import tifffile as tiff
 import time
-import multiprocessing
+import threading
 import os
+import multiprocessing as mp
 start = time.process_time()
 
 #variables
@@ -14,22 +15,20 @@ boolAll = 1
 
 picNames = ["rgb", "r", "g", "b", "g1", "g2"]
 
+name = "Picture1"
+
 
 m = 0.015
 
-n = 8
+n = 1
 
-manager = multiprocessing.Manager()
-list1 = manager.list()
-print(list1)
 output = []
 image_position = "fits/first_fit.fit"
 
 lowPercentClip = 0
 highPercentClip = 100
 
-for i in range(n):
-    list1.append(0)
+tOutput = []
 
 
 #check if resolution is divisible by 2 and is odd
@@ -64,23 +63,28 @@ def mappingFunction(x, m):
 #RGB Transform and Stretch OUTPUT ALL
 #[0] = coloured [1] = red [2] = greenAVG [3] = blue [4] = green1 [5] = green2
 def TransformStretchALL(resx, resy, data, bitIn, m, low, high, y, n):
-    global list1
+    global tOutput
     start = int(((resy/(2*(n)))*y))
     stop = int((resy/(2*(n)))*(y+1))
     #print(start, stop)
-    print("Process: " + str(y))
     output = [[],[],[],[],[],[]]
     fact = 1
     for i in range(start, stop):
     #for i in range(int(resy / 2)):
         xes = [[],[],[],[],[],[]]
         for j in range(int(resx / 2)):
-            preR = data[i*2-1][j*2-1]
-            preG = (data[i*2][j*2-1] + data[i*2-1][j*2])/2
-            preB = data[i*2][j*2]
+            preR = data[i*2][j*2]
+            preG = (data[i*2][j*2+1] + data[i*2+1][j*2])/2
+            preB = data[i*2+1][j*2+1]
             preR = (preR - low)/ (high - low)
             preG = (preG - low)/ (high - low)
             preB = (preB - low)/ (high - low)
+            preG1 = data[i*2][j*2+1]
+            preG2 = data[i*2+1][j*2]
+            preG1 = (preG1 - low)/ (high - low)
+            preG2 = (preG2 - low)/ (high - low)
+            print(preG, preG1, preG2, preR)
+            time.sleep(1)
             if preR <= 0:
                 preR = 0
             if preR >= 1:
@@ -93,9 +97,21 @@ def TransformStretchALL(resx, resy, data, bitIn, m, low, high, y, n):
                 preB = 0
             if preB >= 1:
                 preB = 1
+            if preG1 <= 0:
+                preG1 = 0
+            if preG1 >= 1:
+                preG1 = 1
+            if preG2 <= 0:
+                preG2 = 0
+            if preG2 >= 1:
+                preG2 = 1
             r = mappingFunction(preR*fact, m)*(2**bitn)
             g = mappingFunction(preG*fact, m)*(2**bitn)
             b = mappingFunction(preB*fact, m)*(2**bitn)
+            #g1 = mappingFunction(preG1*fact, m)*(2**bitn)
+            #g2 = mappingFunction(preG2*fact, m)*(2**bitn)
+            g1 = g
+            g2 = g
             
             
             
@@ -103,31 +119,29 @@ def TransformStretchALL(resx, resy, data, bitIn, m, low, high, y, n):
             xes[1].append((r, 0, 0))
             xes[2].append((0, g, 0))
             xes[3].append((0, 0, b))
-            xes[4].append((0, g, 0))
-            xes[5].append((0, g, 0))
+            xes[4].append((0, g1, 0))
+            xes[5].append((0, g2, 0))
             
         if (i % 100) == 0:
             print(i)
         for i in range(len(output)):    
             output[i].append(xes[i])
-    list1[y] = output
-    print(list1[y])
-    return
+    tOutput[y] = output
     
-def TransformStretch(resx, resy, data, bitIn, m, low, high, y, n, q):
+def TransformStretch(resx, resy, data, bitIn, m, low, high, y, n):
+    global tOutput
     start = int(((resy/(2*(n)))*y))
     stop = int((resy/(2*(n)))*(y+1))
     #print(start, stop)
-    print("Process: " + str(y))
     output = []
     fact = 1
     for i in range(start, stop):
     #for i in range(int(resy / 2)):
         xes = []
         for j in range(int(resx / 2)):
-            preR = data[i*2-1][j*2-1]
-            preG = (data[i*2][j*2-1] + data[i*2-1][j*2])/2
-            preB = data[i*2][j*2]
+            preR = data[i*2][j*2]
+            preG = (data[i*2][j*2+1] + data[i*2+1][j*2])/2
+            preB = data[i*2+1][j*2+1]
             preR = (preR - low)/ (high - low)
             preG = (preG - low)/ (high - low)
             preB = (preB - low)/ (high - low)
@@ -147,68 +161,18 @@ def TransformStretch(resx, resy, data, bitIn, m, low, high, y, n, q):
             g = mappingFunction(preG*fact, m)*(2**bitn)
             b = mappingFunction(preB*fact, m)*(2**bitn)
             
+            
+            
             xes.append((r, g, b))
             
         if (i % 100) == 0:
             print(i)
             
         output.append(xes)
-    list1[y] = output
-    return
+    tOutput[y] = output
 
-def multiProcessing(n, boolAll, resx, resy, data, bitIn, m, lowerClip, higherClip):
-    global list1
-    q = multiprocessing.Queue()
-    Processes = []
-    for i in range(n):
-        #list1.append(0)
-        Processes.append(0)
-    final = []
-    prefinal1 = []
-    prefinal2 = []
-    if boolAll == 0:
-        for i in range(n):
-            Processes[i] = multiprocessing.Process(target=TransformStretch, args=(resx, resy, data, bitIn, m,
-                                                                                    lowerClip, higherClip, i, n, q,))
-            print(Processes[i])
-            Processes[i].start()
-            #Processes = nP
-    else:
-        for i in range(n):
-            Processes[i] = multiprocessing.Process(target=TransformStretchALL, args=(resx, resy, data, bitIn, m,
-                                                                                       lowerClip, higherClip, i, n,))
-            print(Processes[i])
-            Processes[i].start()
-    
-    """if boolAll == 0:
-        for i in range(n):
-            queue = None
-            queue = q.get()
-            prefinal1.append(queue)"""
-    """else:
-        for i in range(n):
-            queue = None
-            print(type(q.get()))
-            prefinal2.extend(queue)"""
-    
-    for i in range(n):
-        Processes[i].join()
-        print("Process joined: " + str(i))
-        Processes[i].terminate()
-        
-    if boolAll == 0:
-        final = []
-        for i in range(n):
-            final.extend(prefinal1[i])
-        
-    else:
-        print(list1)
-        final = [[],[],[],[],[],[]]
-        for i in range(len(list1)):
-            for j in range(len(list1[i])):
-                final[j].extend(list1[i][j])
-    return final
-
+def multiProcessing():
+    pass
 
 needed_data()
 
@@ -217,8 +181,7 @@ higherClip = np.percentile(data, highPercentClip)
 print(lowerClip, higherClip)
 
 #output = TransformStretch(resx, resy, data, bitIn, m, lowerClip, higherClip, 0, 4)
-if __name__ == '__main__':
-    output = multiProcessing(n, boolAll, resx, resy, data, bitIn, m, lowerClip, higherClip)
+output = multiThreading(n, boolAll)
 
 
 
@@ -227,6 +190,7 @@ if boolAll == 0:
 else:
     outArray = [[],[],[],[],[],[]]
     for i in range(len(output)):
+        print(i)
         outArray[i] = np.array(output[i], "uint" + str(bitn))
 
 #print(outArray)
@@ -243,7 +207,7 @@ else:
         os.mkdir(directory)
         print("Directory " , directory ,  " Created ") 
         for i in range(len(picNames)):
-            tiff.imwrite("ALL" + str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + 'finite.tif' + "/" + picNames[i] + "_" + str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + 'finite.tif', outArray[i], photometric = "rgb")
+            tiff.imwrite("ALL" + str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + 'finite.tif' + "/" + picNames[i] + "_" + str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + name + '.tif', outArray[i], photometric = "rgb")
     except FileExistsError:
         print("Directory " , directory ,  " already exists")
 print(time.process_time() - start)    
