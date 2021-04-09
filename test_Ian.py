037963eb58a4c75efec0f233cc4411d90a322215
KikatoMikishi <ian.the.wim@gmail.com>
2021-04-09 15:40:59 +0200

    schoan

--- test_Ian.py
+++ test_Ian.py
@@ -2,14 +2,13 @@ from astropy.io import fits
 import numpy as np
 import tifffile as tiff
 import time
-import threading
 import os
-import multiprocessing as mp
+import multiprocessing
 start = time.process_time()
 
 #variables
-bitn = 16
-bitIn = 16
+bitn = 8
+bitIn = 8
 
 boolAll = 1
 
@@ -20,7 +19,7 @@ name = "Picture1"
 
 m = 0.015
 
-n = 1
+n = 8
 
 output = []
 image_position = "fits/first_fit.fit"
@@ -28,7 +27,11 @@ image_position = "fits/first_fit.fit"
 lowPercentClip = 0
 highPercentClip = 100
 
-tOutput = []
+mpQueue = multiprocessing.Queue()
+Processes = []
+
+for i in range(n):
+    Processes.append(0)
 
 
 #check if resolution is divisible by 2 and is odd
@@ -62,7 +65,7 @@ def mappingFunction(x, m):
 
 #RGB Transform and Stretch OUTPUT ALL
 #[0] = coloured [1] = red [2] = greenAVG [3] = blue [4] = green1 [5] = green2
-def TransformStretchALL(resx, resy, data, bitIn, m, low, high, y, n):
+def TransformStretchALL(resx, resy, data, bitIn, m, low, high, y, n, mpQueue):
     global tOutput
     start = int(((resy/(2*(n)))*y))
     stop = int((resy/(2*(n)))*(y+1))
@@ -83,8 +86,8 @@ def TransformStretchALL(resx, resy, data, bitIn, m, low, high, y, n):
             preG2 = data[i*2+1][j*2]
             preG1 = (preG1 - low)/ (high - low)
             preG2 = (preG2 - low)/ (high - low)
-            print(preG, preG1, preG2, preR)
-            time.sleep(1)
+            #print(preG, preG1, preG2, preR)
+            #time.sleep(1)
             if preR <= 0:
                 preR = 0
             if preR >= 1:
@@ -126,9 +129,9 @@ def TransformStretchALL(resx, resy, data, bitIn, m, low, high, y, n):
             print(i)
         for i in range(len(output)):    
             output[i].append(xes[i])
-    tOutput[y] = output
+    mpQueue.put(output)
     
-def TransformStretch(resx, resy, data, bitIn, m, low, high, y, n):
+def TransformStretch(resx, resy, data, bitIn, m, low, high, y, n, mpQueue):
     global tOutput
     start = int(((resy/(2*(n)))*y))
     stop = int((resy/(2*(n)))*(y+1))
@@ -160,54 +163,100 @@ def TransformStretch(resx, resy, data, bitIn, m, low, high, y, n):
             r = mappingFunction(preR*fact, m)*(2**bitn)
             g = mappingFunction(preG*fact, m)*(2**bitn)
             b = mappingFunction(preB*fact, m)*(2**bitn)
-            
-            
-            
             xes.append((r, g, b))
             
         if (i % 100) == 0:
             print(i)
             
         output.append(xes)
-    tOutput[y] = output
+    mpQueue.put(output)
 
-def multiProcessing():
-    pass
-
-needed_data()
-
-lowerClip = np.percentile(data, lowPercentClip)
-higherClip = np.percentile(data, highPercentClip)
-print(lowerClip, higherClip)
-
-#output = TransformStretch(resx, resy, data, bitIn, m, lowerClip, higherClip, 0, 4)
-output = multiThreading(n, boolAll)
-
-
-
-if boolAll == 0:
-    outArray = np.array(output, "uint" + str(bitn))
-else:
-    outArray = [[],[],[],[],[],[]]
-    for i in range(len(output)):
-        print(i)
-        outArray[i] = np.array(output[i], "uint" + str(bitn))
-
-#print(outArray)
-print(time.process_time() - start)
-start = time.process_time()
-
-if boolAll == 0:
-    tiff.imwrite(str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + 'finite.tif', outArray, photometric='rgb')
+def multiProcessing(resx, resy, data, bitIn, m, lowerClip, higherClip, n, boolAll):
+    global Processes, mpQueue
+    results = []
+    for i in range(n):
+        Processes[i] = multiprocessing.Process(target=TransformStretch, args=(resx, resy, data, bitIn, m, lowerClip,
+                                                                   higherClip, i, n, mpQueue,))
+        Processes[i].start()
+    for i in range(n):
+        Processes[i].start()
     
-else:
-    directory = "ALL" + str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + 'finite.tif'
-    try:
-        # Create target Directory
-        os.mkdir(directory)
-        print("Directory " , directory ,  " Created ") 
-        for i in range(len(picNames)):
-            tiff.imwrite("ALL" + str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + 'finite.tif' + "/" + picNames[i] + "_" + str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + name + '.tif', outArray[i], photometric = "rgb")
-    except FileExistsError:
-        print("Directory " , directory ,  " already exists")
-print(time.process_time() - start)    
+    for i in range(n):
+        results.extend(mpQueue.get())
+        
+    for p in Processes:
+        p.join()
+        p.terminate()
+        print("Process " + str(p) + " finished")
+    return results
+    print("Done with multiprocessing")
+
+
+
+def multiProcessingAll(resx, resy, data, bitIn, m, lowerClip, higherClip, n):
+    global Processes, mpQueue
+    results = []
+    final = [[],[],[],[],[],[]]
+    for i in range(n):
+        Processes[i] = multiprocessing.Process(target=TransformStretchALL, args=(resx, resy, data, bitIn, m, lowerClip,
+                                                                                  higherClip, i, n, mpQueue,))
+        Processes[i].start()
+    print("All Processes started")
+    for i in range(n):
+        results.extend(mpQueue.get())
+    print("got informations")
+    for i in range(len(results)):
+        for j in range(len(results[i])):
+            final[j].extend(results[i][j])
+    print("edited List")
+    for p in Processes:
+        p.join()
+        p.terminate()
+        print("Process " + str(p) + " finished")
+    return final
+    print("Done with multiprocessing")
+    
+    
+if __name__ == '__main__':
+
+    needed_data()
+
+    lowerClip = np.percentile(data, lowPercentClip)
+    higherClip = np.percentile(data, highPercentClip)
+    print(lowerClip, higherClip)
+
+    #output = TransformStretch(resx, resy, data, bitIn, m, lowerClip, higherClip, 0, 4)
+    if boolAll == 0:
+        output = multiProcessing(resx, resy, data, bitIn, m, lowerClip, higherClip, n)
+    else:
+        output = multiProcessingAll(resx, resy, data, bitIn, m, lowerClip, higherClip, n)
+
+
+
+    if boolAll == 0:
+        outArray = np.array(output, "uint" + str(bitn))
+    else:
+        outArray = [[],[],[],[],[],[]]
+        for i in range(len(output)):
+            print(i)
+            outArray[i] = np.array(output[i], "uint" + str(bitn))
+
+    #print(outArray)
+    print(time.process_time() - start)
+    start = time.process_time()
+
+    if boolAll == 0:
+        tiff.imwrite(str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + 'finite.tif', outArray, photometric='rgb')
+        
+    else:
+        directory = "ALL" + str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + 'finite.tif'
+        try:
+            # Create target Directory
+            os.mkdir(directory)
+            print("Directory " , directory ,  " Created ") 
+            for i in range(len(picNames)):
+                tiff.imwrite("ALL" + str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + 'finite.tif' + "/" + picNames[i] + "_" + str(lowPercentClip) + "_" + str(highPercentClip) + "_m" + str(m) + "_" + str(bitn) + "bit_" + name + '.tif', outArray[i], photometric = "rgb")
+        except FileExistsError:
+            print("Directory " , directory ,  " already exists")
+    print(time.process_time() - start)    
+    print("done")
